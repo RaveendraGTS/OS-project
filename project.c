ef0adc59207a596984f9abba2dcdfd16d2d7cad9
Pabbisetty G T S Raveendra <43849514+RaveendraGTS@users.noreply.github.com>
2019-04-05 19:57:57 +0530

    Add files via upload

--- project.c
+++ project.c
@@ -1,20 +1,27 @@
 #include<stdio.h>
 int main()
 {
-    int BT[20];
-    int pr[20];
-    int WT[20];
-    int TAT[20];
+    int BT[200];
+    int pr[200];
+    int WT[200];
+    int TAT[200];
+    int PRIOR[200];
+    int d[200];
     int i,j,n,pos,temp;
     int TOTAL=0,TATTOTAL=0;
     float av_wt,av_tat;
     printf("enter no of process\n");
     scanf("%d",&n);
-    printf("enter the value of burst time\n");
+    printf("enter the value of burst time\n");//enter burst time
     for(i=0;i<n;i++)
     {
         printf("p%d:",i+1);
         scanf("%d",&BT[i]);
+        if(BT[i]<=0)
+        {
+            printf("entered the wrong value\n");
+            return 0;
+        }
         pr[i]=i+1;
     }
     for(i=0;i<n;i++)
@@ -34,29 +41,32 @@ int main()
         pr[pos]=temp;
     }
     WT[0]=0;
-    for(i=1;i<n;i++)
+    for(i=1;i<n;i++) //calculating waiting time
     {
         WT[i]=0;
         for(j=0;j<i;j++)
         WT[i]+=BT[j];
-        TOTAL+=WT[i];
+        TOTAL += WT[i];
     }
-    av_wt=(float)TOTAL/n;
-    printf("total waiting time=%d",av_wt);
-    for(i=0;i<n;i++)
+
+    for(i=0;i<n;i++)//calculating turn around time
     {
         TAT[i] = BT[i] + WT[i];
         TATTOTAL+=TAT[i];
     }
-    av_tat=TATTOTAL/n;
+    for(i=0;i<n;i++)//calculating priority
+    {
+        d[i]=WT[i]/BT[i];
+        PRIOR[i] = 1 + d[i];
+    }
     printf("\n====================================================================================");
-    printf("\nPROCESS  \tBURST TIME \tWAITING TIME \tTURN AROUND TIME");
+    printf("\nPROCESS  \tBURST TIME \tWAITING TIME \tTURN AROUND TIME \t PRIORITY");
     printf("\n-------------------------------------------------------------------------------------");
     for(i=0;i<n;i++)
     {
-        printf("\np%d\t\t%d\t\t%d\t\t%d\t\t",pr[i],BT[i],WT[i],TAT[i]);
+        printf("\np%d\t\t%d\t\t%d\t\t%d\t\t\t%d\t\t",pr[i],BT[i],WT[i],TAT[i],PRIOR[i]);
     }
     printf("\n====================================================================================");
-    printf("\naverage time=\n%f",&av_wt);
-    printf("\naverage turn around time=\n%f",&av_tat);
+    printf("\naverage waiting time=\n%f",(float)TOTAL/n);
+    printf("\naverage turn around time=\n%f",(float)TATTOTAL/n);
 }
